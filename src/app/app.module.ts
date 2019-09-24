297cd4a526c932b58a7754eba0077a827bb41c44
Christian Alexis Cruz Arango <christiancruzarango@gmail.com>
2019-09-24 17:54:30 -0500

    modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboradComponent } from './pages/dashborad/dashborad.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegistrarComponent } from './login/registrar/registrar.component';
 
 import { APPROUTING } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { RegistrarComponent } from './login/registrar/registrar.component';
+
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboradComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegistrarComponent
   ],
   imports: [
     BrowserModule,
-    APPROUTING
+    APPROUTING,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
